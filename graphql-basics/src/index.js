74558809ce1594fc6076423e1097176e1763a744
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-12-22 21:10:26 -0600

    completes challenge to bring export two math functions

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,6 +1,9 @@
 import myCurrentLocation, { message, name, greeting } from "./myModule";
+import Add, { subtractNumbers } from "./math";
 
 const student = "Jessica";
 
 console.log(`${name} says ${message} and he lives in ${myCurrentLocation}`);
 console.log(greeting(student));
+console.log(Add(1, 1));
+console.log(subtractNumbers(10, 1));
---
+++ graphql-basics/src/math.js
@@ -0,0 +1,9 @@
+const addNumbers = (a, b) => {
+  return a + b;
+};
+
+const subtractNumbers = (a, b) => {
+  return a - b;
+};
+
+export { addNumbers as default, subtractNumbers };
